4e4ef8bd6ed53d6a352305b9b1d504984c72d9de
Bodunrin Ladele <bladele@me.com>
2012-09-17 22:33:35 -0400

    Meaningful commit for Monday

---
+++ Week4/B/js/json.js
@@ -0,0 +1,182 @@
+var json = {
+	"loveLog1": {
+		"eventTypes": ["Event Types: ", "Agape[of the soul]"],
+		"title": ["Title: ", "My First Log"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "06/12/2012"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "85"],
+		"notes": ["The Details: ", "Watching the sunrise this morning was breathtaking."]
+	},
+	"loveLog2": {
+		"eventTypes": ["Event Types: ", "Eros[of passion]"],
+		"title": ["Title: ", "She's so beautiful!"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "05/01/2012"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "90"],
+		"notes": ["The Details: ", "I find she is the most beautiful when she isn't trying so hard."]
+	},
+	"loveLog3": {
+		"eventTypes": ["Event Types: ", "Philia[of the mind]"],
+		"title": ["Title: ", "Learning To Code"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "07/04/2012"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "86"],
+		"notes": ["The Details: ", "I wasn't sure about how well I would do in the MDV program @ FSU. I'm learning a lot."]
+	},
+	"loveLog4": {
+		"eventTypes": ["Event Types: ", "Philia[of the mind]"],
+		"title": ["Title: ", "The Matrix Rocks"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "04/29/2012"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "99"],
+		"notes": ["The Details: ", "Such a great movie, which completly ingulfs your imagination."]
+	},
+	"loveLog5": {
+		"eventTypes": ["Event Types: ", "Eros[of passion]"],
+		"title": ["Title: ", "Bought My Wife a Gift"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "08/01/2011"],
+		"role": ["Role: ", "donor"],
+		"wow": ["Wow Factor: ", "85"],
+		"notes": ["The Details: ", "Jenny is going to be so jazzed about what I got her for her birthday!"]
+	},
+	"loveLog6": {
+		"eventTypes": ["Event Types: ", "Agape[of the soul]"],
+		"title": ["Title: ", "Dogs are so cute."],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "10/20/2012"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "70"],
+		"notes": ["The Details: ", "Sometime, I wonder what my dogs are thinking."]
+	},
+	"loveLog7": {
+		"eventTypes": ["Event Types: ", "Agape[of the soul]"],
+		"title": ["Title: ", "Gardot is an inspiration."],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "09/13/2012"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "95"],
+		"notes": ["The Details: ", "Was listening to Melody Gardot's new album. Love it!"]
+	},
+	"loveLog8": {
+		"eventTypes": ["Event Types: ", "Agape[of the soul]"],
+		"title": ["Title: ", "The Sky"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "10/20/2011"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "70"],
+		"notes": ["The Details: ", "Sunsets rock."]
+	},
+	"loveLog9": {
+		"eventTypes": ["Event Types: ", "Philia[of the mind]"],
+		"title": ["Title: ", "Star Trek"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "12/30/2011"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "99"],
+		"notes": ["The Details: ", "Hands down the great TV franchise ever created."]
+	},
+	"loveLog10": {
+		"eventTypes": ["Event Types: ", "Philia[of the mind]"],
+		"title": ["Title: ", "Radio Lab"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "11/13/2011"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "90"],
+		"notes": ["The Details: ", "Nothing other form audio entertainment sparks the imagination more."]
+	},
+	"loveLog11": {
+		"eventTypes": ["Event Types: ", "Philia[of the mind]"],
+		"title": ["Title: ", "NPR"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "10/26/2011"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "89"],
+		"notes": ["The Details: ", "The only way to get your news, there's no BS."]
+	},
+	"loveLog12": {
+		"eventTypes": ["Event Types: ", "Storge[parental]"],
+		"title": ["Title: ", "Cuddling with the kids"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "04/23/12"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "85"],
+		"notes": ["The Details: ", "There is nothing like the feeling of your kids cuddling with you on the couch."]
+	},
+	"loveLog13": {
+		"eventTypes": ["Event Types: ", "Storge[parental]"],
+		"title": ["Title: ", "So proud"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "03/10/12"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "85"],
+		"notes": ["The Details: ", "Listen to Audrey sing while playing the piano make me so proud."]
+	},
+	"loveLog14": {
+		"eventTypes": ["Event Types: ", "Storge[parental]"],
+		"title": ["Title: ", "Questions, questions."],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "01/04/12"],
+		"role": ["Role: ", "Donor"],
+		"wow": ["Wow Factor: ", "85"],
+		"notes": ["The Details: ", "My son's curiosity is growing, but it is exhausting 'cause he expects I have all the answers."]
+	},
+	"loveLog15": {
+		"eventTypes": ["Event Types: ", "Storge[parental]"],
+		"title": ["Title: ", "Ayo's Birthday."],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "12/21/11"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "80"],
+		"notes": ["The Details: ", "Wow, he's already 4 years old. Where does the time go?"]
+	},
+	"loveLog16": {
+		"eventTypes": ["Event Types: ", "Storge[parental]"],
+		"title": ["Title: ", "Audrey's gift."],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "11/25/11"],
+		"role": ["Role: ", "Donor"],
+		"wow": ["Wow Factor: ", "88"],
+		"notes": ["The Details: ", "Can't wait to give Audrey her Christmas gift, the look on her face will priceless."]
+	},
+	"loveLog17": {
+		"eventTypes": ["Event Types: ", "Xenia[hospitality]"],
+		"title": ["Title: ", "Saturday at the Bormann party house."],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "08/14/12"],
+		"role": ["Role: ", "Recipient"],
+		"wow": ["Wow Factor: ", "96"],
+		"notes": ["The Details: ", "With aboundant food and drink, and the pool, we always have fun as family there."]
+	},
+	"loveLog18": {
+		"eventTypes": ["Event Types: ", "Xenia[hospitality]"],
+		"title": ["Title: ", "Dinner at the Browns'"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "04/14/12"],
+		"role": ["Role: ", "Donor"],
+		"wow": ["Wow Factor: ", "83"],
+		"notes": ["The Details: ", "Dinner at the Browns was a lot of fun. Jenny and I enjoyed cooking for them."]
+	},
+	"loveLog19": {
+		"eventTypes": ["Event Types: ", "Xenia[hospitality]"],
+		"title": ["Title: ", "Brinley and Zoe"],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "02/17/12"],
+		"role": ["Role: ", "Donor"],
+		"wow": ["Wow Factor: ", "75"],
+		"notes": ["The Details: ", "Ayo always likes having those two over. And the are always so polite."]
+	},
+	"loveLog20": {
+		"eventTypes": ["Event Types: ", "Philia[of the mind]"],
+		"title": ["Title: ", "Conversations with Niran."],
+		"location": ["Location: ", "Concord, NC"],
+		"date": ["Date: ", "03/19/12"],
+		"role": ["Role: ", "Donor"],
+		"wow": ["Wow Factor: ", "78"],
+		"notes": ["The Details: ", "Had another long conversation with Niran today. We always get into the most philosophical topics."]
+	}
+}
